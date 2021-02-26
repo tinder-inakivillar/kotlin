5218601102d02096d6a9ee4a1a36fb9827b0d8c2
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2021-02-26 17:11:24 +0100

    Replace, where possible in tests, jcenter() with mavenCentral().
    In few places I have to leave bintray repository due to not all kotlin
    dependencies where migrated to maven central.

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/KotlinGradlePluginIT.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/KotlinGradlePluginIT.kt
@@ -1097,7 +1097,7 @@ class KotlinGradleIT : BaseGradleIT() {
         build(
             ":lib1:compileDebugUnitTestKotlin",
             options = defaultBuildOptions().copy(
-                androidGradlePluginVersion = AGPVersion.v3_2_0,
+                androidGradlePluginVersion = AGPVersion.v3_6_0,
                 androidHome = KtTestUtil.findAndroidSdk(),
             ),
         ) {
