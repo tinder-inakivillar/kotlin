1dffffd79bac45d9e32c4aa535e3c3193db82b8a
Andrei Klunnyi <andrei.klunnyi@jetbrains.com>
2021-03-02 15:06:18 +0100

    [FIR]: sealed hierarchy processor for IDE // review: test amendment

--- idea/idea-frontend-fir/idea-fir-low-level-api/tests/org/jetbrains/kotlin/idea/fir/low/level/api/AbstractFirSealedInheritorsTest.kt
+++ idea/idea-frontend-fir/idea-fir-low-level-api/tests/org/jetbrains/kotlin/idea/fir/low/level/api/AbstractFirSealedInheritorsTest.kt
@@ -5,57 +5,27 @@
 
 package org.jetbrains.kotlin.idea.fir.low.level.api
 
-import com.google.gson.JsonElement
-import com.google.gson.JsonObject
-import com.intellij.openapi.vfs.VirtualFileManager
-import com.intellij.psi.PsiManager
-import org.jetbrains.kotlin.executeOnPooledThreadInReadAction
 import org.jetbrains.kotlin.fir.declarations.*
-import org.jetbrains.kotlin.idea.fir.low.level.api.api.getOrBuildFirOfType
-import org.jetbrains.kotlin.idea.fir.low.level.api.api.getResolveState
-import org.jetbrains.kotlin.idea.jsonUtils.getString
-import org.jetbrains.kotlin.idea.stubs.AbstractMultiModuleTest
-import org.jetbrains.kotlin.idea.util.sourceRoots
-import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.test.KotlinTestUtils
 import org.jetbrains.kotlin.test.util.KtTestUtil
 import org.jetbrains.kotlin.types.typeUtil.closure
+import org.jetbrains.kotlin.utils.addToStdlib.flatMapToNullable
 import java.io.File
-import java.nio.file.Paths
 
 /**
  * The idea behind this test is to check that [FirIdeSealedHierarchyProcessor] finds all direct inheritors of sealed classes and interfaces.
  * We use the fact that [SealedClassInheritorsKt#getSealedInheritors] property gets its value thanks to the class activity.
  *
  * Inheritors are collected for every sealed declaration of the 'fileToResolve' (see test data 'structure.json'). Resulting collection is
- * compared with 'expected.txt' ignoring the order.
+ * compared with 'expected.txt'.
  */
-abstract class AbstractFirSealedInheritorsTest : AbstractMultiModuleTest() {
+abstract class AbstractFirSealedInheritorsTest : AbstractFirMultiModuleLazyResolveTest() {
     override fun getTestDataPath(): String =
         "${KtTestUtil.getHomeDirectory()}/idea/idea-frontend-fir/idea-fir-low-level-api/testdata/resolveSealed/"
 
-    fun doTest(path: String) {
-        val testStructure = TestProjectStructureForSealed.fromTestProjectStructure(
-            TestProjectStructureReader.read(Paths.get(path))
-        )
-
-        val testProjectModule = testStructure.module
-        val moduleToResolve = module(testProjectModule.name)
-
-        val fileToAnalysePath = moduleToResolve.sourceRoots.first().url + "/" + testStructure.fileToResolve.relativeFilePath
-
-        val virtualFileToAnalyse = VirtualFileManager.getInstance().findFileByUrl(fileToAnalysePath)
-            ?: error("File ${testStructure.fileToResolve.filePath} not found")
-        val ktFileToAnalyse = PsiManager.getInstance(project).findFile(virtualFileToAnalyse) as KtFile
-        val resolveState = ktFileToAnalyse.getResolveState()
-
-        val inheritorIds = executeOnPooledThreadInReadAction {
-            val firFile = ktFileToAnalyse.getOrBuildFirOfType<FirFile>(resolveState)
-            val allClasses = firFile.listNestedClasses().closure { it.listNestedClasses() }
-            allClasses.flatMap { it.sealedInheritors ?: emptyList() }.toList()
-        }
-
-        val inheritorNames = inheritorIds?.map { it.asString() }?.toList()?.sorted() ?: emptyList()
+    override fun checkFirFile(firFile: FirFile, path: String) {
+        val allClasses = firFile.listNestedClasses().closure { it.listNestedClasses() }
+        val inheritorNames = allClasses.flatMap { it.sealedInheritors ?: emptyList() }.map { it.asString() }.sorted()
         KotlinTestUtils.assertEqualsToFile(File("$path/expected.txt"), inheritorNames.joinToString("\n"))
     }
 }
@@ -65,37 +35,5 @@ private fun FirDeclaration.listNestedClasses(): List<FirRegularClass> {
         is FirFile -> declarations.filterIsInstance<FirRegularClass>()
         is FirRegularClass -> declarations.filterIsInstance<FirRegularClass>()
         else -> emptyList()
-    }
-}
-
-private data class FileToResolveForSealed(val moduleName: String, val relativeFilePath: String) {
-    val filePath get() = "$moduleName/$relativeFilePath"
-
-    companion object {
-        fun parse(json: JsonElement): FileToResolveForSealed {
-            require(json is JsonObject)
-            return FileToResolveForSealed(
-                moduleName = json.getString("module"),
-                relativeFilePath = json.getString("file")
-            )
-        }
-    }
-}
-
-private data class TestProjectStructureForSealed(
-    val module: TestProjectModule,
-    val fileToResolve: FileToResolveForSealed
-) {
-    companion object {
-        fun fromTestProjectStructure(testProjectStructure: TestProjectStructure): TestProjectStructureForSealed {
-            if (testProjectStructure.modules.size > 1)
-                throw IllegalStateException("Multi module projects for sealed resolution are not supported so far")
-
-            val module = testProjectStructure.modules.first()
-            val json = testProjectStructure.json
-            val fileToResolve = FileToResolveForSealed.parse(json.getAsJsonObject("fileToResolve"))
-
-            return TestProjectStructureForSealed(module, fileToResolve)
-        }
     }
 }
